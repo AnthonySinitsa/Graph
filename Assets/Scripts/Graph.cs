20401ac9ec4469f10ef00bd76061ce107d2611d1
Anton <itonyemail@gmail.com>
2023-12-13 00:25:28 -0800

    animated graph by adding time

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -17,7 +17,6 @@ public class Graph : MonoBehaviour{
         for(int i = 0; i < points.Length; i++){
             Transform point = points[i] = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
-            // position.y = position.x * position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
@@ -27,6 +26,9 @@ public class Graph : MonoBehaviour{
     void Update() {
         for (int i = 0; i < points.Length; i++){
             Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
         }
     }
 }
