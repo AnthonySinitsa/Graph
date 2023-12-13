cb2709635548b0c22aec81d23f989aaf7d73bc55
Anton <itonyemail@gmail.com>
2023-12-13 14:02:56 -0800

    animating ripple method

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -7,7 +7,7 @@ public class Graph : MonoBehaviour{
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
-    [SerializeField, Range(0, 1)]
+    [SerializeField, Range(0, 2)]
     int function;
 
     Transform[] points;
