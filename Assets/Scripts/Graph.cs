493961d93c8b1cb5855bb0259a08d98ccec08098
Anton <itonyemail@gmail.com>
2023-12-15 00:23:48 -0800

    Update Graph.cs

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -16,11 +16,13 @@ public class Graph : MonoBehaviour{
     TransitionMode transitionMode;
 
     [SerializeField, Min(0f)]
-    float functionDuration = 1f;
+    float functionDuration = 1f, transitionDuration = 1f;
 
     Transform[] points;
 
     float duration;
+    bool transitioning;
+    FunctionLibrary.FunctionName transitionFunction;
 
     void Awake(){
         float step = 2f / resolution;
@@ -39,7 +41,12 @@ public class Graph : MonoBehaviour{
             duration -= functionDuration;
             PickNextFunction();
         }
-        UpdateFunction();
+
+        if(transitioning){
+             UpdateFunctionTransition();
+        }else{
+            UpdateFunction();
+        }
     }
 
     void UpdateFunction(){
@@ -55,6 +62,27 @@ public class Graph : MonoBehaviour{
             }
             float u = (x + 0.5f) * step - 1f;
             points[i].localPosition = f(u, v, time);
+        }
+    }
+
+    void UpdateFunctionTransition(){
+        FunctionLibrary.Function 
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++){
+            if(x == resolution){
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(
+                u, v, time, from, to, progress
+            );
         }
     }
 
